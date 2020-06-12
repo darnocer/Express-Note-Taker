5440f26d62b06df445553e81088905dc60a1fc99
darnocer <darnocer@gmail.com>
2020-06-11 19:59:01 -0600

    working post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,32 @@
 const router = require("express").Router();
+const notesData = require("../db/db.json");
+const fs = require("fs");
 
 router.get("/notes", function (req, res) {
-  console.log(res.send("GET request"));
+  res.json(notesData);
 });
 
 router.post("/notes", function (req, res) {
-  console.log(res.send("POST request"));
+  const id = new Date();
+  notesData.push({ id, ...req.body });
+  res.json(true);
+
+  fs.writeFileSync("db/db.json", JSON.stringify(notesData), function (err) {
+    if (err) {
+      throw err;
+    }
+  });
 });
 
 router.delete("/notes/:id", function (req, res) {
-  console.log(res.send("POST request"));
+  notesData.forEach(function (item, index, arr) {
+    if (item.id === req.params.id) {
+      arr.splice(index, 1);
+      res.send();
+      return;
+    }
+  });
+  res.status(404).send();
 });
 
 module.exports = router;
